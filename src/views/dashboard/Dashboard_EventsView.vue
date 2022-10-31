e5493e120d66afbdc61a4230b75166933565ecc6
ChristianSegundo <christian.segundomatilde@stud.fhgr.ch>
2022-10-31 10:04:46 +0100

    test

--- src/views/dashboard/Dashboard_EventsView.vue
+++ src/views/dashboard/Dashboard_EventsView.vue
@@ -2,7 +2,7 @@
   <FirstComponent></FirstComponent>
   <h1>Dashboard_EventsView - Spiesser</h1>
 </template>
-
+<p>ich bin Christian</p>
 <script>
 // @ is an alias to /src
 import FirstComponent from '@/components/FirstComponent.vue'
