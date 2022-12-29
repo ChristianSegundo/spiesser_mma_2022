c539bcfba0add148f7b8099c8ecb877fab5ceddb
Kevin Adrian <114489179+KevinAdrian9@users.noreply.github.com>
2022-12-29 14:01:51 +0100

    Update Placeholder bei Mitbringsel

--- src/views/organisator/Event_Erstellen_04View.vue
+++ src/views/organisator/Event_Erstellen_04View.vue
@@ -24,7 +24,7 @@
   <!-- <input type="text" class="mitbringliste-input" placeholder="2 Gascho Bier"> -->
 
   <div class="mitbringliste-input">
-    <input type="text" class="storage" :placeholder="aktuelles_dingsel" v-model="aktuelles_dingsel">
+    <input type="text" class="storage" placeholder="z.B. 2 Gascho Bier" v-model="aktuelles_dingsel">
 
     <button class='button' @click = "push_ding()">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
@@ -50,7 +50,7 @@ export default {
   },
   data(){
     return{
-      aktuelles_dingsel: "z.B. 2 Gascho Bier",
+      aktuelles_dingsel: "",
       mitbringseldinge: [],
       new_event
     }
